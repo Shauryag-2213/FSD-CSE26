de890e6c112b96fc5bba08c88ad35050b55b58ab
Shauryag-2213 <shauryagoel2005@gmail.com>
2026-09-10 09:27:49 +0530

    Created the basic frontend of the site

---
+++ BookApp/client/src/components/Book.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Book.css";
+const Book = () => {
+    return (
+        <div className="book">
+            <img src = "" alt="Book Image"/>
+            <h3>Title:ReactJS</h3>
+            <h3>Price: $45</h3>
+            <button>Add To Cart</button>
+        </div>
+    )
+}
---
+++ BookApp/client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <div className="header">
+            <h1>My Shopping List</h1>
+        </div>
+    )
+}
+
+export default Header
